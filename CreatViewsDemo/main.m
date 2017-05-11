1a998ddb9ee98f17a8a2986137b1a8b5e96967c5
阿浩 <ah@AHdeMacBook-Pro.local>
2017-05-11 17:47:09 +0800

    Initial Commit

---
+++ CreatViewsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CreatViewsDemo
+//
+//  Created by AH on 2017/5/11.
+//  Copyright © 2017年 AH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
